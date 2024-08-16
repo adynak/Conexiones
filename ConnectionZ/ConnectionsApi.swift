8be32ed49f13de856ac071d1c8a9d030ea5cc9ad
Al Dynak <adynak@gmail.com>
2024-08-16 11:14:15 -0700

    change to github url

--- ConnectionZ/ConnectionsApi.swift
+++ ConnectionZ/ConnectionsApi.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 struct ConnectionsApi {
-  static func fetchBy(date: String) async -> GameData? {
-    let url = URL(string: "https://www.nytimes.com/svc/connections/v1/\(date).json")!
-    do {
-      let (data, _) = try await URLSession.shared.data(from: url)
-      return try JSONDecoder().decode(GameData.self, from:data)
-    } catch {
-      return nil
+    static func fetchBy(date: String) async -> GameData? {
+        let url = URL(string: "https://adynak.github.io/conexiones/puzzles/\(date).json")!
+//        let url = URL(string: "https://www.nytimes.com/svc/connections/v1/\(date).json")!
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return try JSONDecoder().decode(GameData.self, from:data)
+        } catch {
+            return nil
+        }
     }
-  }
 }
