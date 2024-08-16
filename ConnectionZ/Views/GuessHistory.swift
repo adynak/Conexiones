0ad4a09a9655d3b0b7c417ef50b7e6d8ad0ee5f7
Al Dynak <adynak@gmail.com>
2024-08-16 12:50:37 -0700

    separating views

---
+++ ConnectionZ/Views/GuessHistory.swift
@@ -0,0 +1,18 @@
+//
+//  GuessHistory.swift
+//  ConnectionZ
+//
+//  Created by al00p on 8/16/24.
+//
+
+import SwiftUI
+
+struct GuessHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuessHistory()
+}
