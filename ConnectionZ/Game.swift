8a71d562350d0f91c17ea5c212779902f13d49b7
Al Dynak <adynak@gmail.com>
2024-08-16 11:49:03 -0700

    localization is fun

--- ConnectionZ/Game.swift
+++ ConnectionZ/Game.swift
@@ -9,140 +9,143 @@ import Foundation
 import SwiftData
 
 struct Group: Codable {
-  let name: String
-  let level: Int
-  let words: Set<String>
-  
-  func emoji() -> String {
-    switch (level) {
-    case 0: return "🟨"
-    case 1: return "🟩"
-    case 2: return "🟦"
-    case 3: return "🟪"
-    default: return "⬛️"
+    let name: String
+    let level: Int
+    let words: Set<String>
+    
+    func emoji() -> String {
+        switch (level) {
+        case 0: return "🟨"
+        case 1: return "🟩"
+        case 2: return "🟦"
+        case 3: return "🟪"
+        default: return "⬛️"
+        }
     }
-  }
-  
-  func score(of guess: Set<String>) -> Int {
-    return self.words.intersection(guess).count
-  }
+    
+    func score(of guess: Set<String>) -> Int {
+        return self.words.intersection(guess).count
+    }
 }
 
 struct Guess: Codable {
-  let words: Set<String>
-  let score: Int
-  
-  var id: Set<String> { return words }
-  
-  init(words: Set<String>, score: Int) {
-    self.words = words
-    self.score = score
-  }
+    let words: Set<String>
+    let score: Int
+    
+    var id: Set<String> { return words }
+    
+    init(words: Set<String>, score: Int) {
+        self.words = words
+        self.score = score
+    }
 }
 
 enum GuessResult {
-  case alreadyGuessed
-  case incorrect
-  case oneAway
-  case correct
+    case alreadyGuessed
+    case incorrect
+    case oneAway
+    case correct
 }
 
 @Model class Game {
-  @Attribute(.unique) let id: Int
-  @Attribute(.unique) let date: String
-  let groups: [Group]
-  var words: [String]
-  var guesses = [Guess]()
-  
-  init(id: Int, date: String, words: [String], groups: [Group]) {
-    self.id = id
-    self.date = date
-    self.words = words
-    self.groups = groups
-  }
-  
-  convenience init(from gameData: GameData, on date: String) {
-    let words = gameData.startingGroups.flatMap {$0}
-    let groups = gameData.groups.map { (groupName, groupData) in
-      return Group(name: groupName, level: groupData.level, words: Set(groupData.members))
+    @Attribute(.unique) let id: Int
+    @Attribute(.unique) let date: String
+    let groups: [Group]
+    var words: [String]
+    var guesses = [Guess]()
+    
+    init(id: Int, date: String, words: [String], groups: [Group]) {
+        self.id = id
+        self.date = date
+        self.words = words
+        self.groups = groups
     }
-    self.init(id: gameData.id, date: date, words: words, groups: groups)
-  }
-  
-  func reset() -> Void {
-    self.guesses.removeAll()
-    self.words = self.groups.flatMap { $0.words }.shuffled()
-  }
-  
-  var name: String {
-    return "Puzzle #\(Game.puzzleNumber(for: Date(iso8601:self.date)))"
-  }
+    
+    convenience init(from gameData: GameData, on date: String) {
+        let words = gameData.startingGroups.flatMap {$0}
+        let groups = gameData.groups.map { (groupName, groupData) in
+            return Group(name: groupName, level: groupData.level, words: Set(groupData.members))
+        }
+        self.init(id: gameData.id, date: date, words: words, groups: groups)
+    }
+    
+    func reset() -> Void {
+        self.guesses.removeAll()
+        self.words = self.groups.flatMap { $0.words }.shuffled()
+    }
+    
+    var name: String {
+        let puzzleLabel = NSLocalizedString("Puzzle",comment: "label Puzzle")
 
-  var foundGroups: [Group] {
-    let correctGuesses = guesses.filter{ guess in return guess.score == 4}
-    return correctGuesses.map { guess in
-      return groups.first { group in return group.words == guess.words }!
-    }
-  }
-  
-  var numFoundGroups: Int {
-    return foundGroups.count
-  }
-  
-  var isComplete: Bool {
-    return self.words.isEmpty
-  }
-  
-  var isInProgress: Bool {
-    return !self.guesses.isEmpty && !self.isComplete
-  }
-  
-  var emojis: String {
-    let emojis = guesses.map { guess in
-      guess.words.sorted().map { word in
-        self.groups.first { $0.words.contains(word) }!.emoji()
-      }.joined(separator: "")
-    }.joined(separator: "\n")
-    return ["ConnectionZ", self.name, emojis].joined(separator: "\n")
-  }
-  
-  func guess(words candidates: Set<String>) -> GuessResult {
-    if guesses.contains(where: { $0.words == candidates }) {
-      return .alreadyGuessed
+        
+        return "\(puzzleLabel) #\(Game.puzzleNumber(for: Date(iso8601:self.date)))"
     }
     
-    let closestGroup: Group = self.groups.reduce(self.groups[0]) {acc, cur in
-      let accCount = acc.score(of: candidates)
-      let curCount = cur.score(of: candidates)
-      return curCount > (accCount) ? cur : acc
+    var foundGroups: [Group] {
+        let correctGuesses = guesses.filter{ guess in return guess.score == 4}
+        return correctGuesses.map { guess in
+            return groups.first { group in return group.words == guess.words }!
+        }
     }
     
-    let score = closestGroup.score(of: candidates)
-    if score == 4 {
-      self.words = self.words.filter { !closestGroup.words.contains($0) }
+    var numFoundGroups: Int {
+        return foundGroups.count
     }
     
-    guesses.append(Guess(words: candidates, score: closestGroup.score(of: candidates)))
-    switch score {
-    case 4:
-      return .correct
-    case 3:
-      return .oneAway
-    default:
-      return .incorrect
+    var isComplete: Bool {
+        return self.words.isEmpty
     }
-  }
-  
-  func guess(row indices: Range<Int>) -> GuessResult  {
-    return self.guess(words: Set(self.words[indices]))
-  }
-  
-  func shuffle() {
-    self.words.shuffle()
-  }
-  
-  func hoist(words: Set<String>) {
-    self.words.removeAll(where: { words.contains($0) })
-    self.words.insert(contentsOf: words, at: 0)
-  }
+    
+    var isInProgress: Bool {
+        return !self.guesses.isEmpty && !self.isComplete
+    }
+    
+    var emojis: String {
+        let emojis = guesses.map { guess in
+            guess.words.sorted().map { word in
+                self.groups.first { $0.words.contains(word) }!.emoji()
+            }.joined(separator: "")
+        }.joined(separator: "\n")
+        return ["ConnectionZ", self.name, emojis].joined(separator: "\n")
+    }
+    
+    func guess(words candidates: Set<String>) -> GuessResult {
+        if guesses.contains(where: { $0.words == candidates }) {
+            return .alreadyGuessed
+        }
+        
+        let closestGroup: Group = self.groups.reduce(self.groups[0]) {acc, cur in
+            let accCount = acc.score(of: candidates)
+            let curCount = cur.score(of: candidates)
+            return curCount > (accCount) ? cur : acc
+        }
+        
+        let score = closestGroup.score(of: candidates)
+        if score == 4 {
+            self.words = self.words.filter { !closestGroup.words.contains($0) }
+        }
+        
+        guesses.append(Guess(words: candidates, score: closestGroup.score(of: candidates)))
+        switch score {
+        case 4:
+            return .correct
+        case 3:
+            return .oneAway
+        default:
+            return .incorrect
+        }
+    }
+    
+    func guess(row indices: Range<Int>) -> GuessResult  {
+        return self.guess(words: Set(self.words[indices]))
+    }
+    
+    func shuffle() {
+        self.words.shuffle()
+    }
+    
+    func hoist(words: Set<String>) {
+        self.words.removeAll(where: { words.contains($0) })
+        self.words.insert(contentsOf: words, at: 0)
+    }
 }
--- ConnectionZ/GameView.swift
+++ ConnectionZ/GameView.swift
@@ -10,252 +10,255 @@ import SwiftUI
 import Drops
 
 extension String: Identifiable {
-  public typealias ID = Int
-  public var id: Int {
-    return hash
-  }
+    public typealias ID = Int
+    public var id: Int {
+        return hash
+    }
 }
 
 extension View {
     public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
         let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
         return clipShape(roundedRect)
-             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+            .overlay(roundedRect.strokeBorder(content, lineWidth: width))
     }
 }
 
 struct Tile: View {
-  var word: String
-  var selected: Bool
-  var selectAction: () -> Void
-  
-  var borderWidth: CGFloat {
-    return selected ? 4 : 0
-  }
-  var body: some View {
-    Text(word)
-      .multilineTextAlignment(.center)
-      .lineLimit(1)
-      .minimumScaleFactor(0.1)
-      .padding(8)
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .aspectRatio(1, contentMode: .fit)
-      .bold()
-      .background(
-        RoundedRectangle(
-          cornerSize: CGSize(width: 10, height: 10)
-        )
-        .fill(Color.secondaryBackground)
-        .addBorder(Color.accentColor, width: borderWidth, cornerRadius: 10)
-      ).onTapGesture(perform: selectAction)
-
-  }
+    var word: String
+    var selected: Bool
+    var selectAction: () -> Void
+    
+    var borderWidth: CGFloat {
+        return selected ? 4 : 0
+    }
+    var body: some View {
+        Text(word)
+            .multilineTextAlignment(.center)
+            .lineLimit(1)
+            .minimumScaleFactor(0.1)
+            .padding(8)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .aspectRatio(1, contentMode: .fit)
+            .bold()
+            .background(
+                RoundedRectangle(
+                    cornerSize: CGSize(width: 10, height: 10)
+                )
+                .fill(Color.secondaryBackground)
+                .addBorder(Color.accentColor, width: borderWidth, cornerRadius: 10)
+            ).onTapGesture(perform: selectAction)
+        
+    }
 }
 
 struct CompletedGroup: View {
-  let group: Group
-  
-  var color: Color {
-    switch (group.level) {
-    case 0: return Color(.displayP3, red: 245/256, green: 224/256, blue: 126/256)
-    case 1: return Color(.displayP3, red: 167/256, green: 194/256, blue: 104/256)
-    case 2: return Color(.displayP3, red: 180/256, green: 195/256, blue: 235/256)
-    case 3: return Color(.displayP3, red: 178/256, green: 131/256, blue: 193/256)
-    default: return Color.black
+    let group: Group
+    
+    var color: Color {
+        switch (group.level) {
+        case 0: return Color(.displayP3, red: 245/256, green: 224/256, blue: 126/256)
+        case 1: return Color(.displayP3, red: 167/256, green: 194/256, blue: 104/256)
+        case 2: return Color(.displayP3, red: 180/256, green: 195/256, blue: 235/256)
+        case 3: return Color(.displayP3, red: 178/256, green: 131/256, blue: 193/256)
+        default: return Color.black
+        }
     }
-  }
-  
-  var body: some View {
-    VStack {
-      Text(group.name).font(.title2).bold().foregroundStyle(.black)
-      Text(group.words.sorted().joined(separator: ", ")).foregroundStyle(.black)
+    
+    var body: some View {
+        VStack {
+            Text(group.name).font(.title2).bold().foregroundStyle(.black)
+            Text(group.words.sorted().joined(separator: ", ")).foregroundStyle(.black)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(
+            RoundedRectangle(
+                cornerSize: CGSize(
+                    width: 10,
+                    height: 10
+                )
+            ).fill(color))
     }
-    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-    .background(
-      RoundedRectangle(
-        cornerSize: CGSize(
-          width: 10,
-          height: 10
-        )
-      ).fill(color))
-  }
 }
 
 struct CompletedGroups: View {
-  let groups: [Group]
-  var body: some View {
-    ForEach(groups, id: \.name) { group in
-      CompletedGroup(group: group)
+    let groups: [Group]
+    var body: some View {
+        ForEach(groups, id: \.name) { group in
+            CompletedGroup(group: group)
+        }
     }
-  }
 }
 
 struct GuessView: View {
-  let i: Int
-  let guess: Guess
-  
-  var icon: String {
-    if guess.score == 4 {
-      return "4.square.fill"
-    } else if guess.score == 3 {
-      return "3.square"
-    } else {
-      return "square"
+    let i: Int
+    let guess: Guess
+    
+    var icon: String {
+        if guess.score == 4 {
+            return "4.square.fill"
+        } else if guess.score == 3 {
+            return "3.square"
+        } else {
+            return "square"
+        }
     }
-  }
-  
-  var body: some View {
-    GridRow {
-      Label(guess.words.sorted().joined(separator: ", "), systemImage: icon)
+    
+    var body: some View {
+        GridRow {
+            Label(guess.words.sorted().joined(separator: ", "), systemImage: icon)
+        }
     }
-  }
 }
 
 struct GuessesView: View {
-  var guesses: [Guess]
-  var body: some View {
-    Text("Guesses").font(.title3)
-    ScrollView {
-      Grid(alignment: .leading) {
-        ForEach(Array(guesses.enumerated()), id: \.offset) {
-          i, guess in
-          GuessView(i: i, guess: guess)
+    var guesses: [Guess]
+    var body: some View {
+        Text("Guesses").font(.title3)
+        ScrollView {
+            Grid(alignment: .leading) {
+                ForEach(Array(guesses.enumerated()), id: \.offset) {
+                    i, guess in
+                    GuessView(i: i, guess: guess)
+                }
+            }.padding()
         }
-      }.padding()
+        .defaultScrollAnchor(.bottom)
+        .frame(maxWidth: 500, maxHeight: .infinity)
+        .background(
+            RoundedRectangle(
+                cornerSize: CGSize(width: 10, height: 10)
+            )
+            .fill(Color.secondaryBackground)
+        )
     }
-    .defaultScrollAnchor(.bottom)
-    .frame(maxWidth: 500, maxHeight: .infinity)
-    .background(
-      RoundedRectangle(
-        cornerSize: CGSize(width: 10, height: 10)
-      )
-      .fill(Color.secondaryBackground)
-    )
-  }
 }
 
 struct GameView: View {
-  let cols = [
-    GridItem(.flexible(), spacing: 8),
-    GridItem(.flexible(), spacing: 8),
-    GridItem(.flexible(), spacing: 8),
-    GridItem(.flexible(), spacing: 8)
-  ]
-  var game: Game
-  @State var selected = Set<String>()
-  @State var shouldShowConfirmationDialog = false;
-  
-  func select(word: String) {
-    if selected.contains(word) {
-      selected.remove(word)
-    } else if selected.count < 4 {
-      selected.insert(word)
-    }
-  }
-  
-  func isSelected(word: String) -> Bool {
-    return selected.contains(word)
-  }
-  
-  var guessButtonText: String {
-    return selected.count == 4 ? "Guess Selection" : "Guess Top Row"
-  }
-  
-  var body: some View {
-    VStack {
-      GuessesView(guesses: game.guesses)
-      if game.isComplete {
-        Text("Complete!").font(.title2)
-      } else {
-        Text("Create four groups of four!").font(.title2)
-      }
-      VStack(spacing: 8) {
-        CompletedGroups(groups: game.foundGroups)
-        LazyVGrid(columns: cols, spacing: 8, content: {
-          ReorderableForEach(items: game.words) { word in
-            Tile(word: word, selected: isSelected(word: word), selectAction: {
-              select(word: word)
-            })
-          } moveAction: { from, to in
-            game.words.move(fromOffsets: from, toOffset: to)
-          }
-        })
-      }
-      .frame(minWidth: 300, maxWidth: 500, minHeight: 300, maxHeight: 500)
-      .aspectRatio(1, contentMode: .fit)
-      .layoutPriority(1)
-      HStack {
-        if game.isComplete {
-          Spacer()
-          Button("Copy Results") {
-            copyToClipboard(game.emojis)
-            Toast.copied()
-          }.buttonStyle(.bordered)
-          Spacer()
-          Button("Reset Game") {
-            shouldShowConfirmationDialog = true
-          }.buttonStyle(.bordered)
-            .confirmationDialog(
-              "Resetting will delete this game's history.",
-              isPresented: $shouldShowConfirmationDialog
-            ) {
-              Button("Reset Game", role: .destructive) {
-                game.reset()
-              }.keyboardShortcut(.defaultAction)
-              Button("Cancel", role: .cancel, action: {})
-                .keyboardShortcut(.cancelAction)
-            }
-          Spacer()
-        } else {
-          Spacer()
-          Button("Deselect All") {
-            selected.removeAll()
-          }.buttonStyle(.bordered)
-          Spacer()
-          Button("Hoist") {
-            withAnimation {
-              game.hoist(words: selected)
-            }
-            withAnimation {
-              selected.removeAll()
-            }
-          }.buttonStyle(.bordered)
-          Spacer()
-          Button(guessButtonText) {
-            withAnimation {
-              let guessResult = 
-              selected.count == 4 ?
-                game.guess(words: selected) :
-                game.guess(row: 0..<4)
-              switch guessResult {
-              case .alreadyGuessed:
-                Toast.alreadyGuessed()
-              case .oneAway:
-                Toast.oneAway()
-              case .correct:
-                selected.removeAll()
-              case .incorrect:
-                break
-              }
-            }
-          }.buttonStyle(.bordered)
-          Spacer()
+    let cols = [
+        GridItem(.flexible(), spacing: 8),
+        GridItem(.flexible(), spacing: 8),
+        GridItem(.flexible(), spacing: 8),
+        GridItem(.flexible(), spacing: 8)
+    ]
+    var game: Game
+    @State var selected = Set<String>()
+    @State var shouldShowConfirmationDialog = false;
+    
+    func select(word: String) {
+        if selected.contains(word) {
+            selected.remove(word)
+        } else if selected.count < 4 {
+            selected.insert(word)
         }
-      }
-      .padding(.top)
     }
-    .padding()
-  }
+    
+    func isSelected(word: String) -> Bool {
+        return selected.contains(word)
+    }
+    
+    var guessButtonText: String {
+        let guessSelection = NSLocalizedString("GuessSelection",comment: "button: guess selection")
+        let guessTopRow = NSLocalizedString("GuessTopRow",comment: "button guess top row")
+        
+        return selected.count == 4 ? "Guess Selection" : "Guess Top Row"
+    }
+    
+    var body: some View {
+        VStack {
+            GuessesView(guesses: game.guesses)
+            if game.isComplete {
+                Text("Complete!").font(.title2)
+            } else {
+                Text("Create four groups of four!").font(.title2)
+            }
+            VStack(spacing: 8) {
+                CompletedGroups(groups: game.foundGroups)
+                LazyVGrid(columns: cols, spacing: 8, content: {
+                    ReorderableForEach(items: game.words) { word in
+                        Tile(word: word, selected: isSelected(word: word), selectAction: {
+                            select(word: word)
+                        })
+                    } moveAction: { from, to in
+                        game.words.move(fromOffsets: from, toOffset: to)
+                    }
+                })
+            }
+            .frame(minWidth: 300, maxWidth: 500, minHeight: 300, maxHeight: 500)
+            .aspectRatio(1, contentMode: .fit)
+            .layoutPriority(1)
+            HStack {
+                if game.isComplete {
+                    Spacer()
+                    Button("Copy Results") {
+                        copyToClipboard(game.emojis)
+                        Toast.copied()
+                    }.buttonStyle(.bordered)
+                    Spacer()
+                    Button("Reset Game") {
+                        shouldShowConfirmationDialog = true
+                    }.buttonStyle(.bordered)
+                        .confirmationDialog(
+                            "Resetting will delete this game's history.",
+                            isPresented: $shouldShowConfirmationDialog
+                        ) {
+                            Button("Reset Game", role: .destructive) {
+                                game.reset()
+                            }.keyboardShortcut(.defaultAction)
+                            Button("Cancel", role: .cancel, action: {})
+                                .keyboardShortcut(.cancelAction)
+                        }
+                    Spacer()
+                } else {
+                    Spacer()
+                    Button("Deselect All") {
+                        selected.removeAll()
+                    }.buttonStyle(.bordered)
+                    Spacer()
+                    Button("Hoist") {
+                        withAnimation {
+                            game.hoist(words: selected)
+                        }
+                        withAnimation {
+                            selected.removeAll()
+                        }
+                    }.buttonStyle(.bordered)
+                    Spacer()
+                    Button(guessButtonText) {
+                        withAnimation {
+                            let guessResult =
+                            selected.count == 4 ?
+                            game.guess(words: selected) :
+                            game.guess(row: 0..<4)
+                            switch guessResult {
+                            case .alreadyGuessed:
+                                Toast.alreadyGuessed()
+                            case .oneAway:
+                                Toast.oneAway()
+                            case .correct:
+                                selected.removeAll()
+                            case .incorrect:
+                                break
+                            }
+                        }
+                    }.buttonStyle(.bordered)
+                    Spacer()
+                }
+            }
+            .padding(.top)
+        }
+        .padding()
+    }
 }
 
 #Preview {
-  let gameData = GameData(json: "{\"id\":151,\"groups\":{\"DOCTORS’ ORDERS\":{\"level\":0,\"members\":[\"DIET\",\"EXERCISE\",\"FRESH AIR\",\"SLEEP\"]},\"EMAIL ACTIONS\":{\"level\":1,\"members\":[\"COMPOSE\",\"FORWARD\",\"REPLY ALL\",\"SEND\"]},\"PODCASTS\":{\"level\":2,\"members\":[\"RADIOLAB\",\"SERIAL\",\"UP FIRST\",\"WTF\"]},\"___ COMEDY\":{\"level\":3,\"members\":[\"BLACK\",\"DIVINE\",\"PROP\",\"SKETCH\"]}},\"startingGroups\":[[\"COMPOSE\",\"DIVINE\",\"EXERCISE\",\"SEND\"],[\"FRESH AIR\",\"FORWARD\",\"SERIAL\",\"SKETCH\"],[\"WTF\",\"PROP\",\"UP FIRST\",\"DIET\"],[\"BLACK\",\"RADIOLAB\",\"SLEEP\",\"REPLY ALL\"]]}")
-  let game = Game(from: gameData, on: "2023-09-09")
-//  game.guess(words: Set(["RADIOLAB", "UP FIRST", "WTF", "FORWARD"]))
-//  game.guess(words: Set(["RADIOLAB", "UP FIRST", "WTF", "REPLY ALL"]))
-//  game.guess(words: Set(["RADIOLAB", "UP FIRST", "WTF", "SERIAL"]))
-//  game.guess(words: Set(["FORWARD", "COMPOSE", "REPLY ALL", "SEND"]))
-//  game.guess(words: Set(["DIVINE", "PROP", "BLACK", "SKETCH"]))
-//  game.guess(words: Set(["EXERCISE", "FRESH AIR", "DIET", "SLEEP"]))
-  return GameView(game: game).frame(width: 300, height: 600)
+    let gameData = GameData(json: "{\"id\":151,\"groups\":{\"DOCTORS’ ORDERS\":{\"level\":0,\"members\":[\"DIET\",\"EXERCISE\",\"FRESH AIR\",\"SLEEP\"]},\"EMAIL ACTIONS\":{\"level\":1,\"members\":[\"COMPOSE\",\"FORWARD\",\"REPLY ALL\",\"SEND\"]},\"PODCASTS\":{\"level\":2,\"members\":[\"RADIOLAB\",\"SERIAL\",\"UP FIRST\",\"WTF\"]},\"___ COMEDY\":{\"level\":3,\"members\":[\"BLACK\",\"DIVINE\",\"PROP\",\"SKETCH\"]}},\"startingGroups\":[[\"COMPOSE\",\"DIVINE\",\"EXERCISE\",\"SEND\"],[\"FRESH AIR\",\"FORWARD\",\"SERIAL\",\"SKETCH\"],[\"WTF\",\"PROP\",\"UP FIRST\",\"DIET\"],[\"BLACK\",\"RADIOLAB\",\"SLEEP\",\"REPLY ALL\"]]}")
+    let game = Game(from: gameData, on: "2023-09-09")
+    //  game.guess(words: Set(["RADIOLAB", "UP FIRST", "WTF", "FORWARD"]))
+    //  game.guess(words: Set(["RADIOLAB", "UP FIRST", "WTF", "REPLY ALL"]))
+    //  game.guess(words: Set(["RADIOLAB", "UP FIRST", "WTF", "SERIAL"]))
+    //  game.guess(words: Set(["FORWARD", "COMPOSE", "REPLY ALL", "SEND"]))
+    //  game.guess(words: Set(["DIVINE", "PROP", "BLACK", "SKETCH"]))
+    //  game.guess(words: Set(["EXERCISE", "FRESH AIR", "DIET", "SLEEP"]))
+    return GameView(game: game).frame(width: 300, height: 600)
 }
